0ebcacecd60f9c927acb475c749bb5dad2f230d5
Sebastinez <sebastinez@me.com>
2020-06-19 15:35:43 -0300

    host as cli arg and error handling of connection

--- console/console.js
+++ console/console.js
@@ -3,35 +3,38 @@
 const Web3 = require("web3");
 const readline = require("readline");
 
-const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider("http://localhost:4444"));
+const web3 = new Web3(`${process.argv[2]}`);
+console.log(`Establishing connection to ${process.argv[2]}...`);
 
-var rl = null;
+web3.eth
+  .getBlock("latest")
+  .catch((err) => {
+    console.log(`Connection could not be established. ${err.message}`);
+    process.exit(0);
+  })
+  .finally(() => {
+    console.log(`Connected to ${process.argv[2]}`);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.setPrompt("> ");
+    rl.prompt();
 
-rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.setPrompt("RSK > ");
-rl.prompt();
-
-rl.on("line", (line) => {
-  if (line === "quit") rl.close();
-  try {
-    if (!web3.eth.net.isListening())
-      console.log("Connection could not be established. Please try again.");
-    else {
-      var result = eval(line);
-      if (result != null) {
-        console.log(result);
+    rl.on("line", (line) => {
+      if (line === "quit") rl.close();
+      try {
+        var result = eval(line);
+        if (result != null) {
+          console.log(result);
+        }
+      } catch (ex) {
+        console.log("Error during execution of script.");
+        console.log(ex);
       }
-    }
-  } catch (ex) {
-    console.log("Error during execution of script.");
-    console.log(ex);
-  }
 
-  rl.prompt();
-}).on("close", function () {
-  process.exit(0);
-});
+      rl.prompt();
+    }).on("close", function () {
+      process.exit(0);
+    });
+  });
